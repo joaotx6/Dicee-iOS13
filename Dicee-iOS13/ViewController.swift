e3101ab21f97369122280a9a919e9ea34e035258
Joao Teixeira <joaotx66@gmail.com>
2022-11-30 17:06:14 +0000

    working with randoms on diceimgviewq

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,21 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var imgView1: UIImageView!
     @IBOutlet weak var imgView2: UIImageView!
     
-    var leftDiceNumber = 0
-    var rightDiceNumber = 5
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        imgView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [leftDiceNumber]
-        imgView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [rightDiceNumber]
-
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
-
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        //formas iguais de gerar random
+        imgView1.image = diceArray.randomElement()
+        imgView2.image = diceArray[Int.random(in: 0...5)]
     }
 }
 
